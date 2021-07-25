42018978346cef9879b6cf2a10b870140b0cc329
Haeeeun <hegfs6327@naver.com>
2021-07-25 19:03:57 +0900

    use useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -6,20 +6,20 @@ function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
-function App() {
+function App({ onDoSomething }) {
   const [inputs, setInputs] = useState({
     username: '',
     email: '',
   });
   const { username, email } = inputs;
 
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs, // 스프레드 연산자 --> 0부터 끝까지 반복분을 돌겠다.
       [name]: value
     })
-  }
+  }, [inputs]); // Inputs 가 바뀔 때에만 함수가 만들어지고 그렇지 않을 때에는 기존에 있는 함수를 사용하게 된다.
 
   const [users, setUsers] = useState([
     {
@@ -44,12 +44,12 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email,
-    };
+    }
 
     //배열에 새로운 항목을 추가하는 두가지 방법
     setUsers([...users, user]);
@@ -61,19 +61,19 @@ function App() {
       email: '',
     });
     nextId.current += 1;
-  }
+  },[username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id))
-  }
+  },[users]);
 
-  const onToggle = id => {
+  const onToggle = useCallback(id => {
     setUsers(users.map(
       user => user.id === id
       ? {...user, active: !user.active}
         : user
     ))
-  }
+  },[users]);
 
   //users가 바뀔 때에만 호출이 되고 그렇지 않으면 전에 계산된 값을 사용한다. 최적화할 때 사용
   const count = useMemo(() => countActiveUsers(users), [users]);
